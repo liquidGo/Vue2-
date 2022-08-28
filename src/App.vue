2a2ad59826cbebde7be8834abc5a2f9a6538a711
aaaddf <eee@qq.com>
2022-08-28 09:10:46 +0800

    导航栏

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <home-view></home-view>
+  </div>
+</template>
+
+<script>
+import HomeView from './pages/home/Home.vue'
+export default {
+  name: 'App',
+  components: {
+    HomeView
+  }
+}
+</script>
+
+<style lang="less">
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <header-vue></header-vue>
+    </div>
+</template>
+
+<script>
+import HeaderVue from './components/Header.vue';
+export default {
+    name: 'Home',
+    components: {
+        HeaderVue
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
