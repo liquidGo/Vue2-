0bd8f58b4e78845ed4a49ec56ad37aa2c8603f50
aaaddf <eee@qq.com>
2022-08-29 16:25:39 +0800

    search fined

--- src/pages/home/City/City2.vue
+++ src/pages/home/City/City2.vue
@@ -1,19 +1,22 @@
 <template>
     <div>
         <header-city></header-city>
+        <search-vue></search-vue>
         123123
     </div>
 </template>
 
 <script>
 import HeaderCity from './components/Header.vue'
+import SearchVue from './components/Search.vue';
 export default {
     name: 'City',
     mounted() {
         console.log('dayinle');
     },
     components: {
-        HeaderCity
+        HeaderCity,
+        SearchVue
     }
 }
 </script>
---
+++ src/pages/home/City/components/Search.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="输入城市名或简写">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+    .search
+        height .72rem
+        padding 0 .1rem
+        background: #00bcd4;
+        .search-input
+            width 100%
+            line-height .62rem
+            text-align center
+            border-radius .1rem
+</style>
