d111cf405cce947a76b9b11e2fe76b046c6291e3
aaaddf <eee@qq.com>
2022-09-01 19:26:06 +0800

    详情完成

--- src/pages/Detail/Detail.vue
+++ src/pages/Detail/Detail.vue
@@ -1,22 +1,29 @@
 <template>
     <div>
         <banner-vue></banner-vue>
+        <header-vue></header-vue>
+        <div class="content">
+
+        </div>
     </div>
 </template>
 
 <script>
 import BannerVue from './components/Banner.vue'
+import HeaderVue from './components/Header.vue';
 export default {
     name: 'Detail',
     mounted() {
         console.log(this.$route);
     },
-    components:{
-        BannerVue
+    components: {
+        BannerVue,
+        HeaderVue
     }
 }
 </script>
 
 <style lang="stylus" scoped>
-        
+    .content
+        height 50rem
 </style>
